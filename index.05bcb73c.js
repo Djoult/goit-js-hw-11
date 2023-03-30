89447897b0ed4f8831592b88fa14c579c7a3ace8
Djoult <55453354+Djoult@users.noreply.github.com>
2023-03-30 08:27:10 +0000

    Deploying to gh-pages from @ Djoult/goit-js-hw-11@57fc10072d17d7cccbf7398739d1d181ef84898a 🚀

--- index.05bcb73c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();const t=e.target.elements.searchQuery.value;console.log(t)}));
-//# sourceMappingURL=index.05bcb73c.js.map
--- index.256163fd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.target.elements.searchQuery.value;console.log(t)}));
-//# sourceMappingURL=index.256163fd.js.map
